79cb9fb0932edd35ce535d3997736017e1576df1
MillyLegion <legion.3939@mail.ru>
2020-12-18 18:44:16 +0500

    updating our application with reselect, replaced cart selector with memorized selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,23 @@
+// 2 types of selectors: 1. input 2. output
+import { createSelector } from 'reselect';
+
+// getting input
+const selectCart = state => state.cart;
+
+// const selectUser = state => state.user;
+
+export const selectCartItems = createSelector(
+  [selectCart], // input selector
+  // function - returns the value we want from  this selector. gets multiply values
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity, 
+      0
+    )
+);
